f2d737c21e317fa83e94ad8dba48d96849de0c78
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2025-02-02 19:56:15 +0530

    Main server file.

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const connectToDB = require("./database/dbConnection");
+const productRoutes = require("./routes/productRoute");
+const categoryRoutes = require("./routes/categoryRoute");
 const app = express();
 
 app.use(express.json());
+app.use("/api/products", productRoutes);
+app.use("/api/category", categoryRoutes);
 connectToDB();
 
 app.listen(process.env.PORT, () => {
