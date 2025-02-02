40e63a89eb42a35f1f1b4028660a9c9f2d71539c
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2025-02-02 19:55:10 +0530

    created API endpoints for products.

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,7 +1,13 @@
 const express = require("express");
-
+const {
+  addProduct,
+  getProducts,
+  getProductById,
+} = require("../controllers/productController");
 const router = express.Router();
 
-router.post("/add/products");
+router.post("/add/products", addProduct);
+router.get("/products", getProducts);
+router.get("/products/:id", getProductById);
 
 module.exports = router;
