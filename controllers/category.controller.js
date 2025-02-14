6389a17baac5f3414adb58e2b820609b6a2b9e68
Pranay Bhoir <147631196+pranay03bhoir@users.noreply.github.com>
2025-02-14 12:46:14 +0530

    Added CRUD operations function for category.

--- controllers/category.controller.js
+++ controllers/category.controller.js
@@ -16,4 +16,112 @@ const addCategory = async (req, res) => {
     });
   }
 };
-module.exports = { addCategory };
+const updateCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, description } = req.body;
+    const category = await Category.findByIdAndUpdate(id, {
+      name: name,
+      description: description,
+    });
+    if (!category) {
+      return res.status(404).json({
+        success: false,
+        message: "Category not found",
+      });
+    } else {
+      return res.status(200).json({
+        success: true,
+        message: "Category updated successfully",
+        data: category,
+      });
+    }
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong!!",
+    });
+  }
+};
+const getCategories = async (req, res) => {
+  try {
+    const categories = await Category.find({});
+    if (!categories) {
+      return res.status(404).json({
+        success: false,
+        message: "Categories not found",
+      });
+    } else {
+      return res.status(200).json({
+        success: true,
+        message: "Categories found",
+        data: categories,
+      });
+    }
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+};
+const getCategoryByName = async (req, res) => {
+  try {
+    const { name } = req.query;
+    const category = await Category.findOne({
+      name: {
+        $regex: name,
+        $options: "i",
+      },
+    });
+    if (!category) {
+      return res.status(404).json({
+        success: false,
+        message: "Category not found",
+      });
+    } else {
+      return res.status(200).json({
+        success: true,
+        message: "Category found",
+        date: category,
+      });
+    }
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+};
+const deleteCategory = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const category = await Category.findByIdAndDelete(id);
+    if (!category) {
+      return res.status(404).json({
+        success: false,
+        message: "Category does not exists.",
+      });
+    } else {
+      return res.status(200).json({
+        success: true,
+        message: `${category.name} deleted successfully`,
+      });
+    }
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong",
+    });
+  }
+};
+module.exports = {
+  addCategory,
+  updateCategory,
+  getCategories,
+  getCategoryByName,
+};
